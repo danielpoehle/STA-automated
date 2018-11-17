3aaa9c82cdf145e9423df860ba9c3e81aef95806
danielpoehle <d.poehle@gmx.de>
2018-11-17 09:12:00 +0100

    add initial scripts

---
+++ Helper.R
@@ -0,0 +1,60 @@
+# Contents
+# . Helper Functions for File Handling
+# . Helepr Functions for
+
+# install all needed packages if not installed
+helper.neededPackages = c("parallel", "doParallel", "foreach", "lpSolveAPI", "ggplot2", "plyr")
+helper.updatePackages = function() {
+  needed_packages = helper.neededPackages
+  new_packages = needed_packages[!(needed_packages %in% installed.packages()[,"Package"])]
+  if (length(new_packages) > 0) install.packages(new_packages)
+}
+
+# create Folder if not existing
+helper.safeCreateFolder = function (path) {
+  path_points = unlist(strsplit(path, "[/]"))
+  current_path = ''
+  for (pp in path_points) {
+    current_path = paste0(current_path, pp, "/")
+    if (!dir.exists(current_path)) {
+      dir.create(current_path)
+    }
+  }
+
+}
+
+# Create result directory if necessary
+RESULT_PATH = paste0("./result_detail_v", RESULT_VERSION, "/")
+if (!dir.exists(RESULT_PATH)) {
+  dir.create(RESULT_PATH)
+}
+
+helper.getResultPath = function (filename) {
+  return (paste0(RESULT_PATH, filename))
+}
+
+# Progressbar Functions
+#helper.log_socket = helper.getResultPath("log.txt")
+helper.log = function(msg) {
+  print_message = sprintf(paste0(as.character(Sys.time()), ": ", msg, "\n"))
+  cat (print_message)
+
+}
+
+# Check if File Exists, Raise Error if not (error_message = additional error message, if file not found)
+helper.requireFile = function (path, error_message = "") {
+  if (! file.exists(path)) {
+    stop(paste0(path, " not found ! - Please set working directory. ", error_message))
+  }
+}
+
+# if everything works
+helper.updatePackages()
+library("parallel")
+library("doParallel")
+library("foreach")
+library("lpSolveAPI")
+library("ggplot2")
+library("plyr")
+
+helper.included = T
---
+++ main.R
@@ -0,0 +1,98 @@
+# # # # # # # # # # # # # #
+# ! # Change Working directory: Session > Set Working Directory > To Source File Location
+# # #
+
+RESULT_VERSION = '117'
+
+# Configuration Of *relative* paths
+
+STAMMDATEN_FILEPATH = "2013-Stammdaten_ST-STTriebfahrzeugbaureihen121-komplett.xml"
+FAHRLAGEN_FILEPATH = "./2013_Fahrlagen/Fahrlagen_14.11.2013_final_v14.csv"
+FINVEBTS_FILEPATH = "./2013_Fahrlagen/FinVeBTS.csv"
+
+# Configuration for parallel computing
+
+NUMBER_OF_CORES = 7 # Note: It is strongly recommended to keep at least one core unccupied, since RStudio and OS
+
+# Check if everything works by including the heart script:
+
+source("Helper.R")
+
+# # # # # # # # # # # # # #
+# 1 # Mapping Fahrlagen -> STA
+# # #
+
+# Input:
+#STA_FOLDER = "../ZugChar_Untersuchungen/STAs_/"
+WOLKEN_FILEPATH = "2013_Fahrlagen/WolkenBST.csv"
+STA_FOLDER = "./2013_Fahrlagen/ERIKA_STA_181108"
+STA_MAPPING_FILE = "./2013_Fahrlagen/mapping_STAaltneu.csv"
+# Output:
+FAHRLAGEN_STAFIT_FILEPATH = "Fahrlagen_14.11.2013_final_v14_STAFIT.csv"
+BTS2STA_FILEPATH = "bts2sta.csv"
+#STAGROUPS_FILEPATH = "STA_GROUPS_tweaked200.csv"
+STAGROUPS_FILEPATH = "STAGROUPS_v11.csv"
+STA_RESULT_FOLDER = "STAs/"
+# Options:
+DO_OVERLAPPING = F # overlapping currently bugged
+DO_STAFIT = T
+DO_MAPPING_STA_BTS = T
+
+# Execute:
+source("newA-V-MitLaufwegen.R")
+
+
+# # # # # # # # # # # # # #
+# 2 #
+# # #
+
+# Output:
+TEMP_TFZ_FRAME_FILEPATH = "TFZ_Frame.csv"
+# Execute:
+source("analyzeTFZ+WEIGHT.R")
+
+# # # # # # # # # # # # # #
+# 3 # bottomUp_new_a(v)
+# # #
+
+TFZ_LIST_FOR_A_FRAME_FILEPATH = "TFZ_Frame_aFrame.csv"
+TFZ_LIST_DTSTA = "dtSTA.csv"
+BOTTOMUP_RESULT_FOLDER = "all90/"
+A_FRAME_RESULT_FOLDER = "a_frame/"
+
+source("bottomUp_new_a(v).R")
+
+# # # # # # # # # # # # # #
+# 4a # setCoveringOptimizer
+# # ##
+
+BOTTOMUP_REDUCED_RESULT_FOLDER = "all90/reduced/"
+COVERING_RESULT_FOLDER = "bottomup/merge_av/"
+SELECTION_RESULT_FOLDER = "SelectedFiles_Opti_v01.csv"
+
+source("setCoveringOptimizer.R")
+
+# # # # # # # # # # # # # #
+# 4b # Combining two 90% Characteristics
+# # ##
+
+# coming soon
+
+# # # # # # # # # # # # # #
+# 5 # GainMaximizer.R
+# # #
+OPTIMIZATION_RESULT_FOLDER = "optimized"
+source("GainMaximizer.R")
+
+# # # # # # # # # # # # # #
+# 6 # SystemtrassenAssigner.R
+# # #
+
+source("SystemtrassenAssigner.R")
+
+# # # # # # # # # # # # # #
+# 7 # Tagesgang.R
+# # #
+
+CHOSEN_REM = "13" # String!
+source("Tagesgang.R")
